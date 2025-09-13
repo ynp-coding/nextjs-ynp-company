04d30c2e654ca6815fe397569230c0861a385232
onlinepon.dev <onlinepon.dev@gmail.com>
2025-09-14 00:23:07 +0700

    Update Vision & Mission

--- src/app/[locale]/(about)/vision-mission/page.tsx
+++ src/app/[locale]/(about)/vision-mission/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 
 export default function VisionMissionPage() {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto px-8 sm:px-0 md:px-0 lg:px-0">
       <Breadcrumbs
         items={[
           { label: "หน้าหลัก", href: "/home" },
