d6f8daa42b9d87416b6716317d88a30ff2a1d339
onlinepon.dev <onlinepon.dev@gmail.com>
2025-09-18 21:25:23 +0700

    Update Providers

--- src/app/[locale]/providers.tsx
+++ src/app/[locale]/providers.tsx
@@ -1,8 +1,31 @@
 // app/providers.tsx
 "use client";
 
+import { useEffect, useState } from "react";
 import { HeroUIProvider } from "@heroui/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <HeroUIProvider>{children}</HeroUIProvider>;
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="light"
+      enableSystem={false}
+      value={{
+        light: "light",
+        dark: "dark",
+        contrast: "contrast",
+      }}
+    >
+      <HeroUIProvider>{children}</HeroUIProvider>
+    </NextThemesProvider>
+  );
 }
