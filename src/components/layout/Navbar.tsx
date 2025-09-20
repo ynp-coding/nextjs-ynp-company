921a225382f475470d79b6bd0b402089b83a21f0
onlinepon.dev <onlinepon.dev@gmail.com>
2025-09-20 09:41:06 +0700

    Update Navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -30,118 +30,123 @@ export default function Navbar() {
   const services = Object.values(t.raw("navbar.services.links")) as MenuItem[];
 
   return (
-    <NavbarUI maxWidth="2xl" isBordered>
-      <NavbarContent className="hidden sm:flex">
-        <NavbarItem>
-          <Button
-            disableRipple
-            variant="light"
-            onPress={() => router.push("/home")}
-          >
-            {t("navbar.home")}
-          </Button>
-        </NavbarItem>
-
-        <NavbarItem>
-          <Dropdown>
-            <DropdownTrigger>
-              <Button
-                disableRipple
-                variant="light"
-                endContent={<ChevronDown size={16} />}
-              >
-                {t("navbar.about.label")}
-              </Button>
-            </DropdownTrigger>
-            <DropdownMenu aria-label={t("navbar.about.label")} items={aboutUs}>
-              {(item) => (
-                <DropdownItem
-                  key={`${item.id}`}
-                  onPress={() => router.push(item.href)}
-                >
-                  {item.label}
-                </DropdownItem>
-              )}
-            </DropdownMenu>
-          </Dropdown>
-        </NavbarItem>
-
-        <NavbarItem>
-          <Dropdown>
-            <DropdownTrigger>
-              <Button
-                disableRipple
-                variant="light"
-                endContent={<ChevronDown size={16} />}
-              >
-                {t("navbar.news.label")}
-              </Button>
-            </DropdownTrigger>
-            <DropdownMenu aria-label={t("navbar.news.label")} items={news}>
-              {(item) => (
-                <DropdownItem
-                  key={`${item.id}`}
-                  onPress={() => router.push(item.href)}
-                >
-                  {item.label}
-                </DropdownItem>
-              )}
-            </DropdownMenu>
-          </Dropdown>
-        </NavbarItem>
-
-        <NavbarItem>
-          <Dropdown>
-            <DropdownTrigger>
-              <Button
-                disableRipple
-                variant="light"
-                endContent={<ChevronDown size={16} />}
-              >
-                {t("navbar.services.label")}
-              </Button>
-            </DropdownTrigger>
-            <DropdownMenu
-              aria-label={t("navbar.services.label")}
-              items={services}
+    <NavbarUI maxWidth="full" isBordered>
+      <div className="container mx-auto flex items-center justify-between">
+        <NavbarContent className="hidden sm:flex">
+          <NavbarItem>
+            <Button
+              disableRipple
+              variant="light"
+              onPress={() => router.push("/home")}
             >
-              {(item) => (
-                <DropdownItem
-                  key={`${item.id}`}
-                  onPress={() => router.push(item.href)}
+              {t("navbar.home")}
+            </Button>
+          </NavbarItem>
+
+          <NavbarItem>
+            <Dropdown>
+              <DropdownTrigger>
+                <Button
+                  disableRipple
+                  variant="light"
+                  endContent={<ChevronDown size={16} />}
                 >
-                  {item.label}
-                </DropdownItem>
-              )}
-            </DropdownMenu>
-          </Dropdown>
-        </NavbarItem>
+                  {t("navbar.about.label")}
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu
+                aria-label={t("navbar.about.label")}
+                items={aboutUs}
+              >
+                {(item) => (
+                  <DropdownItem
+                    key={`${item.id}`}
+                    onPress={() => router.push(item.href)}
+                  >
+                    {item.label}
+                  </DropdownItem>
+                )}
+              </DropdownMenu>
+            </Dropdown>
+          </NavbarItem>
 
-        <NavbarItem>
-          <Button
-            disableRipple
-            variant="light"
-            onPress={() => router.push("/contact")}
-          >
-            {t("navbar.contact")}
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
+          <NavbarItem>
+            <Dropdown>
+              <DropdownTrigger>
+                <Button
+                  disableRipple
+                  variant="light"
+                  endContent={<ChevronDown size={16} />}
+                >
+                  {t("navbar.news.label")}
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu aria-label={t("navbar.news.label")} items={news}>
+                {(item) => (
+                  <DropdownItem
+                    key={`${item.id}`}
+                    onPress={() => router.push(item.href)}
+                  >
+                    {item.label}
+                  </DropdownItem>
+                )}
+              </DropdownMenu>
+            </Dropdown>
+          </NavbarItem>
 
-      <NavbarContent className="hidden sm:flex" justify="end">
-        <NavbarItem>
-          <Button
-            startContent={
-              <SearchIcon size={16} aria-hidden="true" focusable="false" />
-            }
-            aria-label={t("common.search")}
-            size="sm"
-            isIconOnly
-            color="default"
-            variant="light"
-          />
-        </NavbarItem>
-      </NavbarContent>
+          <NavbarItem>
+            <Dropdown>
+              <DropdownTrigger>
+                <Button
+                  disableRipple
+                  variant="light"
+                  endContent={<ChevronDown size={16} />}
+                >
+                  {t("navbar.services.label")}
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu
+                aria-label={t("navbar.services.label")}
+                items={services}
+              >
+                {(item) => (
+                  <DropdownItem
+                    key={`${item.id}`}
+                    onPress={() => router.push(item.href)}
+                  >
+                    {item.label}
+                  </DropdownItem>
+                )}
+              </DropdownMenu>
+            </Dropdown>
+          </NavbarItem>
+
+          <NavbarItem>
+            <Button
+              disableRipple
+              variant="light"
+              onPress={() => router.push("/contact")}
+            >
+              {t("navbar.contact")}
+            </Button>
+          </NavbarItem>
+        </NavbarContent>
+
+        <NavbarContent className="hidden sm:flex" justify="end">
+          <NavbarItem>
+            <Button
+              startContent={
+                <SearchIcon size={16} aria-hidden="true" focusable="false" />
+              }
+              aria-label={t("common.search")}
+              size="sm"
+              isIconOnly
+              color="default"
+              variant="light"
+            />
+          </NavbarItem>
+        </NavbarContent>
+      </div>
     </NavbarUI>
   );
 }
